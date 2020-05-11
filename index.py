9ea982a32e7cf6af87b0c48db83c4ffaa4253de8
Percyh20 <percyherrera@hotmail.com>
2020-05-11 15:38:53 -0600

    Percy test

--- index.py
+++ index.py
@@ -2,4 +2,14 @@ from number_constants import number_constants
 
 
 def number_to_string(input):
-    return number_constants[input]
+    if input > 20:
+        if input < 100:
+            temp = input // 10
+            temp = temp * 10
+            word = number_constants[temp]
+            res = input - temp
+            word = word + " " + number_constants[res]
+
+            return word
+    else:
+        return number_constants[input]
