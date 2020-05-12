10abd1bd524127271539b1e5e6c32e5e883e0a6f
Percyh20 <percyherrera@hotmail.com>
2020-05-11 18:57:10 -0600

    Percy test2

--- index.py
+++ index.py
@@ -1,15 +1,48 @@
 from number_constants import number_constants
 
 
-def number_to_string(input):
-    if input > 20:
-        if input < 100:
-            temp = input // 10
-            temp = temp * 10
-            word = number_constants[temp]
-            res = input - temp
-            word = word + " " + number_constants[res]
 
-            return word
-    else:
-        return number_constants[input]
+
+def number_to_string(input):
+    work_number = input
+    word = ""
+    while work_number != 0:
+        tempword = ""
+        if work_number >= 1000000:
+            temp = work_number // 1000000
+            tempword = number_to_string(temp)
+            tempword = tempword + " " + number_constants[1000000]
+            temp = temp * 1000000
+            work_number = work_number - temp
+        elif work_number >= 1000:
+            temp = work_number // 1000
+            tempword = number_to_string(temp)
+            tempword = tempword + " " + number_constants[1000]
+            temp = temp * 1000
+            work_number = work_number - temp
+        elif work_number >= 100:
+            temp = work_number // 100
+            tempword = number_to_string(temp)
+            tempword = tempword + " " + number_constants[100]
+            temp = temp * 100
+            work_number = work_number - temp
+        elif 100 > work_number > 20:
+            temp = work_number // 10
+            temp = temp * 10
+            if len(tempword)>0:
+                tempword = tempword + " " + number_constants[temp]
+            else:
+                tempword = number_constants[temp]
+            work_number = work_number - temp
+        elif work_number <= 20:
+            if len(tempword)>0:
+                tempword = tempword + " " + number_constants[work_number]
+            else:
+                tempword = number_constants[work_number]
+            work_number = 0
+        word = word + " " + tempword
+    return word.strip()
+
+
+
+
--- tests.py
+++ tests.py
@@ -9,3 +9,5 @@ class TestNumbersToString(unittest.TestCase):
         self.assertEqual(number_to_string(2), "two")
         self.assertEqual(number_to_string(26), "twenty six")
         self.assertEqual(number_to_string(128), "one hundred twenty eight")
+        self.assertEqual(number_to_string(1528), "one thousand five hundred twenty eight")
+        self.assertEqual(number_to_string(150671), "one hundred fifty thousand six hundred seventy one")
