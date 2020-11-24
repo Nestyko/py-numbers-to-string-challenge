0eb01a2bb9465311515a4ee250c444f1a318f1d3
André de Sousa Araújo <dedeco@gmail.com>
2020-11-24 19:32:46 -0300

    All tests passed but need to add some edgdes cases, like 208

--- index.py
+++ index.py
@@ -2,4 +2,11 @@ from number_constants import number_constants
 
 
 def number_to_string(input):
-    return number_constants[input]
+    numbers = [n for n in str(input)]
+    convert_numbers = []
+    for i, n in zip(reversed(range(len(numbers))), numbers):
+        if 10 ** i >= 100:
+            convert_numbers.append(n)
+        convert_numbers.append(int(n) * 10 ** i)
+    words = [number_constants[int(n)] for n in convert_numbers]
+    return ' '.join(words)
