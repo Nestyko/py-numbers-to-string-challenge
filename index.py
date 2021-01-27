65e08fff45215188ec79e965c3303513d5428b29
christian.lopez@reinamadre.mx <christian.lopez@reinamadre.mx>
2021-01-26 23:53:34 -0600

    [MOD] Exam Christian Lopez

--- index.py
+++ index.py
@@ -1,5 +1,96 @@
 from number_constants import number_constants
 
-
 def number_to_string(input):
-    return number_constants[input]
+    try:
+        if input == 0:
+            return "zero"
+        input = abs(input)
+        if (0 < input and input <= 20) and (input in number_constants):
+            return number_constants[input]
+        else:
+            sOutput = ""
+            sInput = str(input)
+            counter = 0
+            lenght = len(sInput)
+            skip = False
+            if lenght >= 3 and int(sInput[-2::]) in number_constants:
+                counter = 2
+                sOutput = number_to_string(int(sInput[-2::]))
+                for digit in reversed(sInput[:-2]):
+                    if skip:
+                        skip = False
+                        continue
+                    if digit == '0':
+                        counter += 1
+                        continue
+                    tenMult = pow(10,counter)
+                    digit = int(digit)
+                    if counter % 3 == 0 and tenMult in number_constants:
+                        if counter < lenght:
+                            if (sInput[(lenght-counter-2):(lenght-counter)]):
+                                if int(sInput[(lenght-counter-2):(lenght-counter)]) in number_constants:
+                                    if tenMult == 1:
+                                        sOutput = f"{number_constants[int(sInput[(lenght-counter-2):(lenght-counter)])]} {sOutput}"
+                                    else:
+                                        sOutput = f"{number_constants[int(sInput[(lenght-counter-2):(lenght-counter)])]} {number_constants[tenMult]} {sOutput}"
+                                    counter += 2
+                                    skip = True
+                                    continue
+                        if tenMult == 1:
+                            sOutput = f"{number_to_string(digit)} {sOutput}"
+                        else:
+                            sOutput = f"{number_to_string(digit)} {number_constants[tenMult]} {sOutput}"
+                    elif counter % 3 == 0:
+                        if counter % 6 > 2:
+                            sOutput = f"{number_to_string(digit)} {number_constants[tenMult]} {sOutput}"
+                        else:
+                            sOutput = f"{number9_to_string(digit)} {sOutput}"
+                    elif counter % 3 == 1:
+                        sOutput = f"{number_constants[digit*10]} {sOutput}"
+                    elif counter % 3 == 2 and tenMult in number_constants:
+                        sOutput = f"{number_to_string(digit)} {number_constants[tenMult]} {sOutput}"
+                    else:
+                        sOutput = f"{number_to_string(digit*100)} {sOutput}"
+                    counter += 1
+            else:
+                for digit in reversed(sInput):
+                    if skip:
+                        skip = False
+                        continue
+                    if digit == '0':
+                        counter += 1
+                        continue
+                    tenMult = pow(10,counter)
+                    digit = int(digit)
+                    if counter % 3 == 0 and tenMult in number_constants:
+                        if counter < lenght:
+                            if (sInput[(lenght-counter-2):(lenght-counter)]):
+                                if int(sInput[(lenght-counter-2):(lenght-counter)]) in number_constants:
+                                    if tenMult == 1:
+                                        sOutput = f"{number_constants[int(sInput[(lenght-counter-2):(lenght-counter)])]} {sOutput}"
+                                    else:
+                                        sOutput = f"{number_constants[int(sInput[(lenght-counter-2):(lenght-counter)])]} {number_constants[tenMult]} {sOutput}"
+                                    counter += 2
+                                    skip = True
+                                    continue
+                        if tenMult == 1:
+                            sOutput = f"{number_to_string(digit)} {sOutput}"
+                        else:
+                            sOutput = f"{number_to_string(digit)} {number_constants[tenMult]} {sOutput}"
+                    elif counter % 3 == 0:
+                        if counter % 6 > 2:
+                            sOutput = f"{number_to_string(digit)} {number_constants[tenMult]} {sOutput}"
+                        else:
+                            sOutput = f"{number9_to_string(digit)} {sOutput}"
+                    elif counter % 3 == 1:
+                        sOutput = f"{number_constants[digit*10]} {sOutput}"
+                    elif counter % 3 == 2 and tenMult in number_constants:
+                        sOutput = f"{number_to_string(digit)} {number_constants[tenMult]} {sOutput}"
+                    else:
+                        sOutput = f"{number_to_string(digit*100)} {sOutput}"
+                    counter += 1
+            return sOutput
+    except:
+        return "Input is not a valid number"
+
+
--- number_constants.py
+++ number_constants.py
@@ -1,5 +1,5 @@
 number_constants = {
-    0: "zero",
+    # 0: "zero",
     1: "one",
     2: "two",
     3: "three",
@@ -15,11 +15,12 @@ number_constants = {
     13: "thirteen",
     14: "fourteen",
     15: "fifteen",
-    16: "sizteen",
+    16: "sixteen",
     17: "seventeen",
     18: "eighteen",
     19: "nineteen",
     20: "twenty",
+    #26: "twenty six",
     30: "thirty",
     40: "fourty",
     50: "fifty",
@@ -28,6 +29,21 @@ number_constants = {
     80: "eighty",
     90: "ninety",
     100: "hundred",
+    #128: "one hundred twenty eight",
     1000: "thousand",
     1000000: "million",
+    1000000000: "billion",
+    1000000000000: "trillion",
 }
+# 0 |     1 one
+# 1 |     10 ten
+# 2 |     100 hundred
+
+# 3 |     1000 thousand
+# 4 |     10000 ten thousand
+# 5 |     100000 hundred thousand
+
+# 6 |     1000000 million
+# 7 |     10000000 ten million
+# 8 |     100000000 hundred million
+# 9 |     1000000000 billion
